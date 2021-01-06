945010f19106adc02cdd0f14e10d15c49fb64c35
sai_arunesh <vssaiarunesh@gmail.com>
2021-01-06 16:15:22 +0530

    npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#loginModalButton").click(function () {
+      $("#loginModal").modal("toggle");
+    });
+    $("#reservation").click(function () {
+      $("#reserveModal").modal("toggle");
+    });
+
+    $("#mycarousel").carousel({ interval: 1 });
+    $("#carouselButton").click(function () {
+      $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+          $("#mycarousel").carousel("pause");
+          $("#carouselButton").children("span").removeClass("fa-pause");
+          $("#carouselButton").children("span").addClass("fa-play");
+        } else if (
+          $("#carouselButton").children("span").hasClass("fa-play")
+        ) {
+          $("#mycarousel").carousel("cycle");
+          $("#carouselButton").children("span").removeClass("fa-play");
+          $("#carouselButton").children("span").addClass("fa-pause");
+        }
+      });
+    });
+  });
